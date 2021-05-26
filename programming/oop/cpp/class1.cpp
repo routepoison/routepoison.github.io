5165e4843258f5bb2685dfcc414375d1e2c28e54
routepoison <79955377+routepoison@users.noreply.github.com>
2021-05-26 14:39:39 -0400

    COMMIT
    COMMIT

---
+++ programming/oop/cpp/class1.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+using namespace std;
+
+class MyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;  // Attribute (string variable)
+
+};
+
+int main(){
+
+}
